69deee4132094bc36d96a837be719e46a380d5d5
Alex Shao <shao_kai@qq.com>
2017-07-01 18:25:32 +0800

    update

--- src/_tools/map_reduce.py
+++ src/_tools/map_reduce.py
@@ -13,16 +13,27 @@ def map_reduce(task_list, handler, thread_count=0, show_progress=False):
 			task_list[index] = handler(task_list[index])
 			index += thread_count
 			if show_progress:
-				with(lock):
+				with lock:
 					done_count += 1
 					print(done_count, "/", task_count)
 	thread_list = [Thread(target=callback, args=(i,)) for i in range(thread_count)]
 	for thread in thread_list: thread.start()
 	for thread in thread_list: thread.join()
 
+from urllib.request import urlopen
+import re
+
 if __name__ == "__main__":
-	print("hello")
-	task_list = [1,2,3]
-	map_reduce(task_list, lambda i:i+1, show_progress=True)
+	task_list = ["http://baidu.com", "http://sogou.com"]
+	def handler(path):
+		with urlopen(path) as f:
+			#print(f.getcode(), f.info())
+			data = f.read().decode()
+			match = re.search(r'<meta http-equiv="refresh" content="\d+;url=(.+?)">', data)
+			if match:
+				return handler(match.group(1))
+			return data
+
+	map_reduce(task_list, handler, show_progress=True)
 	print(task_list)
 	input()
