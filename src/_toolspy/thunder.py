1e7367ba03e2c3cdcc9bb55fb840c707f83f2798
Alex Shao <shao_kai@qq.com>
2017-07-21 20:49:30 +0800

    update

--- src/_toolspy/thunder.py
+++ src/_toolspy/thunder.py
@@ -1,5 +1,7 @@
 from base64 import b64encode, b64decode
 
+codec = ("utf-8", "gb2312")
+
 THUNDER  = "thunder://"
 FLASHGET = "flashget://"
 QQDL     = "qqdl://"
@@ -8,9 +10,16 @@ def encode(path):
 	path = "AA" + path + "ZZ"
 	return THUNDER + b64encode(path.encode()).decode()
 
-def decode(path, code="utf-8"):
+def decode(path):
 	offset = path.index("://") + 3
-	url = b64decode(path[offset:]).decode(code)
+	url = b64decode(path[offset:])
+	for code in codec:
+		try:
+			url = url.decode(code)
+			break
+		except UnicodeDecodeError as error:
+			pass
+	else: return
 	if path.startswith(THUNDER):
 		return url[2:-2]
 	if path.startswith(FLASHGET):
@@ -19,5 +28,7 @@ def decode(path, code="utf-8"):
 		return url
 
 if __name__ == "__main__":
-	print(decode("thunder://QUFodHRwOi8vZGwxMjAuODBzLmltOjkyMC8xNzA1L8n51q7Qzi/J+dau0M4ubXA0Wlo=", "gb2312"))
+	print(decode("thunder://QUFodHRwOi8vZGwxMjAuODBzLmltOjkyMC8xNzA1L8n51q7Qzi/J+dau0M4ubXA0Wlo="))
+	print(decode("thunder://QUFlZDJrOi8vfGZpbGV8ztLKx8TjtcTR2y43MjBwLkhE1tDX1i5tcDR8MTA3OTUyMTcwNnxGRDE4RDBERUZCMjQ0NTkxNTRGMzY5NkZDRDZBQzJCQnxoPVZWVEFTMk4zRVJDUldGUFhUMlJDWFkyWlVYS1NYNU9UfC9aWg=="))
+	print(decode("thunder://QUFodHRwOi8vYnQuMnR1LmNjL0U5RjFBRTQ1NEFBRTUxNDFEMTYyOTVEREUzQTA4QUE1RkE5NzMwOEYvsNfNw8zHLkJEMTI4MLjfx+XI1dPv1tDX1i5ybXZiWlo="))
 	input()
