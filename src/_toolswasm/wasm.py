198a316f439501b220d836dfbf15b455a7890c4f
Alex Shao <shao_kai@qq.com>
2017-07-26 15:25:59 +0800

    update

--- src/_toolswasm/wasm.py
+++ src/_toolswasm/wasm.py
@@ -1,16 +1,30 @@
 import struct
 import dis
 
-__all__ = ("add", "square")
-
-output = struct.pack("<2I", 0x6d736100, 1)
-
 def add(a: int, b: int) -> int:
 	return a + b
 
 def square(a: int) -> int:
 	return a * a
 
+def genSection(sid, data):
+	return struct.pack("2B", sid, len(data)) + data
+
+def genFuncBody(func):
+	data = b"\x00"
+	for code in dis.Bytecode(func):
+		if code.opname == "LOAD_FAST":
+			data += struct.pack("2B", 0x20, code.arg)
+		elif code.opname == "BINARY_ADD":
+			data += struct.pack("B", 0x6A)
+		elif code.opname == "BINARY_MULTIPLY":
+			data += struct.pack("B", 0x6C)
+		elif code.opname == "RETURN_VALUE":
+			data += struct.pack("B", 0x0F)
+	return data + b"\x0B"
+
+__all__ = ("add", "square")
+output = struct.pack("<2I", 0x6d736100, 1)
 funcInfoList = [globals()[node] for node in __all__]
 
 info = struct.pack("B", len(funcInfoList))
@@ -23,12 +37,12 @@ for func in funcInfoList:
 		info += struct.pack("B", 0x7F)
 	info += struct.pack("B", 1)
 	info += struct.pack("B", 0x7F)
-output += struct.pack("2B", 1, len(info)) + info
+output += genSection(1, info)
 
 info = struct.pack("B", len(funcInfoList))
 for i in range(len(funcInfoList)):
 	info += struct.pack("B", i)
-output += struct.pack("2B", 3, len(info)) + info
+output += genSection(3, info)
 
 info = struct.pack("B", len(funcInfoList))
 for i in range(len(funcInfoList)):
@@ -36,28 +50,15 @@ for i in range(len(funcInfoList)):
 	info += struct.pack("B", len(name))
 	info += name.encode()
 	info += struct.pack("2B", 0, i)
-output += struct.pack("2B", 7, len(info)) + info
+output += genSection(7, info)
 
 info = struct.pack("B", len(funcInfoList))
 for func in funcInfoList:
-	funcInfo = b"\x00"
-	for code in dis.Bytecode(func):
-		if code.opname == "LOAD_FAST":
-			funcInfo += struct.pack("2B", 0x20, code.arg)
-		elif code.opname == "BINARY_ADD":
-			funcInfo += struct.pack("B", 0x6A)
-		elif code.opname == "BINARY_MULTIPLY":
-			funcInfo += struct.pack("B", 0x6C)
-		elif code.opname == "RETURN_VALUE":
-			funcInfo += struct.pack("B", 0x0F)
-	funcInfo += b"\x0B"
-	info += struct.pack("B", len(funcInfo)) + funcInfo
+	data = genFuncBody(func)
+	info += struct.pack("B", len(data)) + data
 
-output += struct.pack("2B", 10, len(info)) + info
+output += genSection(10, info)
 
 print(" ".join([hex(item)[2:].zfill(2) for item in output]))
 
-
-
-#print(ast.dump(tree))
-#input()
+input()
