5b99aec2173b005f77675397eb8007e33427ba24
Alex Shao <shao_kai@qq.com>
2017-07-27 13:03:48 +0800

    revert

--- src/_tools/agal/agalc.py
+++ src/_tools/agal/agalc.py
@@ -12,7 +12,7 @@ class ModLoader():
 		with open(self.path) as f:
 			data = f.read()
 			data = re.sub(r"(?<!\w)(op|oc)(?!\w|\[)", r"\1[0]", data)
-			data = re.sub(r"(?<!\w)(v|op|oc)(\d+)", r"\1[\2]", data)
+			data = re.sub(r"(?<!\w)(va|fs|vc|fc|v|op|oc)(\d+)", r"\1[\2]", data)
 		mod = Mod()
 		exec(compile(data, self.path, "exec"), mod.__dict__)
 		sys.modules[name] = mod
