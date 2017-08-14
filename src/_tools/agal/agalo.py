575779bc6fab28d0396e923caf93b6f37fbbbb73
Alex Shao <shao_kai@qq.com>
2017-08-14 19:07:49 +0800

    update

--- src/_tools/agal/agalo.py
+++ src/_tools/agal/agalo.py
@@ -59,19 +59,13 @@ def parseItem(usageList, line, item, index):
 	item = item[2:i] if i >= 0 else item[2:]
 	usageList[int(item)][line] |= selector << (4 if index == 1 else 0)
 
-def calcUsageList(handler):
-	def wrapper(output_code):
-		usageList = [[0] * len(output_code) for _ in range(8)]
-		[parseItem(usageList, line, code[i], i) for line, code in enumerate(output_code) for i in range(1, len(code))]
-		usageList = [[Info(i, v) for i, v in enumerate(usage) if v > 0] for usage in usageList if sum(usage) > 0]
-		if len(usageList) < 2: return
-		usageList = [list(chain.from_iterable(item.split() for item in usage)) for usage in usageList]
-		assert all(isWriteOnly(v[0]) and isReadOnly(v[-1]) for v in usageList)
-		handler(output_code, usageList)
-	return wrapper
-
-@calcUsageList
-def optimize(output_code, usageList):
+def optimize(output_code):
+	usageList = [[0] * len(output_code) for _ in range(8)]
+	[parseItem(usageList, line, code[i], i) for line, code in enumerate(output_code) for i in range(1, len(code))]
+	usageList = [[Info(i, v) for i, v in enumerate(usage) if v > 0] for usage in usageList if sum(usage) > 0]
+	if len(usageList) < 2: return
+	usageList = [list(chain.from_iterable(item.split() for item in usage)) for usage in usageList]
+	assert all(isWriteOnly(v[0]) and isReadOnly(v[-1]) for v in usageList)
 	index = len(usageList) - 1
 	usedList = findUsedRange(usageList[index])
 	for testRange in unionUsedRange(set(reduce(add, usedList))):
